1977fec2058a65c128276bb0bfa6dfc91fe27509
silasprincipe <53846571+silasprincipe@users.noreply.github.com>
2025-09-26 18:31:22 +0000

    Deploying to gh-pages from @ iobis/resources@cc9caf755bdc91c747662c7e6aaedb223dcca44e 🚀

--- tutorials/duckdb-part1/generate_compact.R
+++ tutorials/duckdb-part1/generate_compact.R
@@ -3,11 +3,19 @@ fe_path <- "/Volumes/OBIS2/obis_20250318_parquet/occurrence/*.parquet"
 full_export <- read_parquet_duckdb(fe_path)
 
 head(full_export)
+
+compact_1 <- full_export |>
+    filter(family == "Acanthuridae") |>
+    select(aphiaid, date_year) |>
+    collect()
+
+write_parquet(compact_1, "compact_1.parquet")
+
 species_id <- c(124316, 145728)
-compact_3 <- full_export |>
+compact_2 <- full_export |>
     filter(aphiaid %in% species_id) |>
     collect()
 
-write_parquet(compact_3, "compact_2.parquet")
+write_parquet(compact_2, "compact_2.parquet")
 
 #zip::unzip("content/tutorials/duckdb-part3/compact_3.parquet.zip", exdir = "occurrence")
